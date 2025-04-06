74a49145ec416168d50d578fcb1d0bbc58775f51
Varun C Kanthety (l0w3Rc4$T) <vc.kanthety@gmail.com>
2025-04-06 18:00:58 +0530

    Segregated CSS and JS code from index.html and saved in styles.css and script.js files

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById("theme-toggle");
+
+    toggleButton.addEventListener("click", function() {
+      document.body.classList.toggle("dark-mode");
+
+      if (document.body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "Switch to Light Mode";
+      } else {
+        toggleButton.textContent = "Switch to Dark Mode";
+      }
+    });
