a017cd3738f7510ba494055727d9cb7f5879fa2a
Varun C Kanthety (l0w3Rc4$T) <vc.kanthety@gmail.com>
2025-04-24 23:40:54 +0530

    Update the page

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const toggleButton = document.getElementById("theme-toggle");
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("theme-toggle");
 
-    toggleButton.addEventListener("click", function() {
-      document.body.classList.toggle("dark-mode");
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
 
-      if (document.body.classList.contains("dark-mode")) {
-        toggleButton.textContent = "Switch to Light Mode";
-      } else {
-        toggleButton.textContent = "Switch to Dark Mode";
-      }
-    });
+    if (document.body.classList.contains("dark-mode")) {
+      toggleButton.textContent = "Switch to Light Mode";
+    } else {
+      toggleButton.textContent = "Switch to Dark Mode";
+    }
+  });
+});
